bad3ab50b02e122384f5d6da736c6ecc6fdecfea
Iván Lewin <57846638+ivanlewin@users.noreply.github.com>
2022-08-09 01:42:47 -0300

    06-2

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,26 +1,27 @@
 // Control Props
 // http://localhost:3000/isolated/exercise/06.js
 
-import * as React from 'react'
-import {Switch} from '../switch'
+import * as React from 'react';
+import { Switch } from '../switch';
+import warning from 'warning';
 
-const callAll = (...fns) => (...args) => fns.forEach(fn => fn?.(...args))
+const callAll = (...fns) => (...args) => fns.forEach(fn => fn?.(...args));
 
 const actionTypes = {
   toggle: 'toggle',
   reset: 'reset',
-}
+};
 
-function toggleReducer(state, {type, initialState}) {
+function toggleReducer(state, { type, initialState }) {
   switch (type) {
     case actionTypes.toggle: {
-      return {on: !state.on}
+      return { on: !state.on };
     }
     case actionTypes.reset: {
-      return initialState
+      return initialState;
     }
     default: {
-      throw new Error(`Unsupported type: ${type}`)
+      throw new Error(`Unsupported type: ${type}`);
     }
   }
 }
@@ -28,60 +29,47 @@ function toggleReducer(state, {type, initialState}) {
 function useToggle({
   initialOn = false,
   reducer = toggleReducer,
-  // 🐨 add an `onChange` prop.
-  // 🐨 add an `on` option here
-  // 💰 you can alias it to `controlledOn` to avoid "variable shadowing."
+  onChange,
+  on: controlledOn,
 } = {}) {
-  const {current: initialState} = React.useRef({on: initialOn})
-  const [state, dispatch] = React.useReducer(reducer, initialState)
-  // 🐨 determine whether on is controlled and assign that to `onIsControlled`
-  // 💰 `controlledOn != null`
+  const { current: initialState } = React.useRef({ on: initialOn });
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const onIsControlled = typeof controlledOn === 'boolean';
 
-  // 🐨 Replace the next line with assigning `on` to `controlledOn` if
-  // `onIsControlled`, otherwise, it should be `state.on`.
-  const {on} = state
+  const previousControlledOn = React.useRef(controlledOn);
+  const on = onIsControlled ? controlledOn : state.on;
 
-  // We want to call `onChange` any time we need to make a state change, but we
-  // only want to call `dispatch` if `!onIsControlled` (otherwise we could get
-  // unnecessary renders).
-  // 🐨 To simplify things a bit, let's make a `dispatchWithOnChange` function
-  // right here. This will:
-  // 1. accept an action
-  // 2. if onIsControlled is false, call dispatch with that action
-  // 3. Then call `onChange` with our "suggested changes" and the action.
+  React.useEffect(() => {
+    warning(!(onIsControlled && onChange === undefined), "Passing on without onChange");
 
-  // 🦉 "Suggested changes" refers to: the changes we would make if we were
-  // managing the state ourselves. This is similar to how a controlled <input />
-  // `onChange` callback works. When your handler is called, you get an event
-  // which has information about the value input that _would_ be set to if that
-  // state were managed internally.
-  // So how do we determine our suggested changes? What code do we have to
-  // calculate the changes based on the `action` we have here? That's right!
-  // The reducer! So if we pass it the current state and the action, then it
-  // should return these "suggested changes!"
-  //
-  // 💰 Sorry if Olivia the Owl is cryptic. Here's what you need to do for that onChange call:
-  // `onChange(reducer({...state, on}, action), action)`
-  // 💰 Also note that user's don't *have* to pass an `onChange` prop (it's not required)
-  // so keep that in mind when you call it! How could you avoid calling it if it's not passed?
+    warning(!((previousControlledOn.current === null || previousControlledOn.current === undefined) && typeof controlledOn === 'boolean'), "Passing undefined or null for on and later passing a value");
+    warning(!(typeof previousControlledOn.current === 'boolean' && (controlledOn === null || controlledOn === undefined)), "Passing a value for on and later passing undefined or null");
 
-  // make these call `dispatchWithOnChange` instead
-  const toggle = () => dispatch({type: actionTypes.toggle})
-  const reset = () => dispatch({type: actionTypes.reset, initialState})
+    previousControlledOn.current = controlledOn;
+  }, [onIsControlled, controlledOn, onChange]);
 
-  function getTogglerProps({onClick, ...props} = {}) {
+  function dispatchWithOnChange(action) {
+    if (onChange) onChange(reducer({ ...state, on }, action), action);
+    if (!onIsControlled) {
+      dispatch(action);
+    }
+  }
+  const toggle = () => dispatchWithOnChange({ type: actionTypes.toggle });
+  const reset = () => dispatchWithOnChange({ type: actionTypes.reset, initialState });
+
+  function getTogglerProps({ onClick, ...props } = {}) {
     return {
       'aria-pressed': on,
       onClick: callAll(onClick, toggle),
       ...props,
-    }
+    };
   }
 
-  function getResetterProps({onClick, ...props} = {}) {
+  function getResetterProps({ onClick, ...props } = {}) {
     return {
       onClick: callAll(onClick, reset),
       ...props,
-    }
+    };
   }
 
   return {
@@ -90,30 +78,30 @@ function useToggle({
     toggle,
     getTogglerProps,
     getResetterProps,
-  }
+  };
 }
 
-function Toggle({on: controlledOn, onChange}) {
-  const {on, getTogglerProps} = useToggle({on: controlledOn, onChange})
-  const props = getTogglerProps({on})
-  return <Switch {...props} />
+function Toggle({ on: controlledOn, onChange }) {
+  const { on, getTogglerProps } = useToggle({ on: controlledOn, onChange });
+  const props = getTogglerProps({ on });
+  return <Switch {...props} />;
 }
 
 function App() {
-  const [bothOn, setBothOn] = React.useState(false)
-  const [timesClicked, setTimesClicked] = React.useState(0)
+  const [bothOn, setBothOn] = React.useState(false);
+  const [timesClicked, setTimesClicked] = React.useState(0);
 
   function handleToggleChange(state, action) {
     if (action.type === actionTypes.toggle && timesClicked > 4) {
-      return
+      return;
     }
-    setBothOn(state.on)
-    setTimesClicked(c => c + 1)
+    setBothOn(state.on);
+    setTimesClicked(c => c + 1);
   }
 
   function handleResetClick() {
-    setBothOn(false)
-    setTimesClicked(0)
+    setBothOn(false);
+    setTimesClicked(0);
   }
 
   return (
@@ -141,12 +129,12 @@ function App() {
         />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 // we're adding the Toggle export for tests
-export {Toggle}
+export { Toggle };
 
 /*
 eslint
